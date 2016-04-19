c7738e90605206b12f4706b99c68da08d41198bd
Mariusz Kubiś <mariusz.kubis.aha@gmail.com>
2016-04-19 13:38:23 +0200

    SLICE-134 Improve SliceBindingsProvider performance

--- slice-aem60/src/main/java/com/cognifide/slice/cq/bindings/CqComponentDefinitionResolver.java
+++ slice-aem60/src/main/java/com/cognifide/slice/cq/bindings/CqComponentDefinitionResolver.java
@@ -1,10 +1,14 @@
 package com.cognifide.slice.cq.bindings;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 
 import com.cognifide.slice.api.provider.ComponentDefinitionResolver;
 import com.day.cq.wcm.api.components.ComponentManager;
@@ -13,14 +17,30 @@ import com.day.cq.wcm.api.components.ComponentManager;
 @Service
 public class CqComponentDefinitionResolver implements ComponentDefinitionResolver {
 
-	@Reference
+	@Reference(cardinality = ReferenceCardinality.OPTIONAL_UNARY)
 	private ComponentManager componentManager;
 
 	@Override
 	public Map<String, Object> getComponentDefinition(String resourceType) {
-		final com.day.cq.wcm.api.components.Component component = componentManager.getComponent(resourceType);
+		if (componentManager != null) {
+			final com.day.cq.wcm.api.components.Component component = componentManager
+					.getComponent(resourceType);
+			if (component != null) {
+				return component.getProperties();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getComponentDefinition(Resource resource) {
+		com.day.cq.wcm.api.components.Component component = resource
+				.adaptTo(com.day.cq.wcm.api.components.Component.class);
 		if (component != null) {
-			return component.getProperties();
+			Map<String, Object> values = component.adaptTo(ValueMap.class);
+			if (values != null) {
+				return new HashMap<String, Object>(values);
+			}
 		}
 		return null;
 	}
