f4e81d75ed38c6deeb91ea7ece7c4c546da5b8c6
Mariusz Kubiś <mariusz.kubis.aha@gmail.com>
2016-04-20 09:15:17 +0200

    SLICE-134 Improve SliceBindingsProvider performance

--- slice-aem60/src/main/java/com/cognifide/slice/cq/bindings/CqComponentDefinitionResolver.java
+++ slice-aem60/src/main/java/com/cognifide/slice/cq/bindings/CqComponentDefinitionResolver.java
@@ -4,21 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 
 import com.cognifide.slice.api.provider.ComponentDefinitionProvider;
-import com.day.cq.wcm.api.components.ComponentManager;
 
 @Component
 @Service
 public class CqComponentDefinitionResolver implements ComponentDefinitionProvider {
-
-	@Reference(cardinality = ReferenceCardinality.OPTIONAL_UNARY)
-	private ComponentManager componentManager;
 
 	@Override
 	public Map<String, Object> getComponentDefinition(Resource resource) {
