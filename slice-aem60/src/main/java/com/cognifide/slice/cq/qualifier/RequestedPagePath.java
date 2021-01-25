ce061e3bc087450f558a35f18178b41c34c42167
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2021-01-25 11:03:17 +0100

    fix: Remove Cognifide references, update logo

--- slice-aem60/src/main/java/com/cognifide/slice/cq/qualifier/RequestedPagePath.java
+++ slice-aem60/src/main/java/com/cognifide/slice/cq/qualifier/RequestedPagePath.java
@@ -1,7 +1,7 @@
 package com.cognifide.slice.cq.qualifier;
 
 /*
- * #%L Slice - CQ Add-on $Id:$ $HeadURL:$ %% Copyright (C) 2012 Cognifide Limited %% Licensed under the Apache
+ * #%L Slice - CQ Add-on $Id:$ $HeadURL:$ %% Copyright (C) 2012 Wunderman Thompson Technology %% Licensed under the Apache
  * License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You
  * may obtain a copy of the License at
  * 
